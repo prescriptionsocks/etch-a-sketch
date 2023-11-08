8ceb7f43aabb807d687a3a66b47aa10395ec29c1
Baraka <barakaarunga@gmail.com>
2023-11-08 13:50:36 +0300

    Rewrote script. Fixed creating grid bug

--- file.js
+++ file.js
@@ -1,43 +1,29 @@
 const gridContainer = document.querySelector('.grid-container');
 
-const number = 72;
+let cellSize = 32;
+let gridSize = cellSize*cellSize;
+console.log(gridSize);
 
-//Add boxes to the container
-function createBoxes() {
-    for(let i = 0; i < number; i++) {
-        const box = document.createElement('div');
-        box.setAttribute('class', 'box');
-        gridContainer.appendChild(box);
-        // make boxes interactive
-        box.addEventListener('mouseenter', ()=> {
-            box.style.backgroundColor="red";
+//get cell size
+function getCellSize () {
+    prompt("Pick a grid size between 1 and 100")
+}
+
+
+//create cells
+function createCells () {
+    for(let i = 0; i < gridSize; i++) {
+        const cell = document.createElement('div');
+        cell.className = "cell";
+        cell.style.width = `${gridContainer.clientWidth/cellSize}px`;
+        cell.style.height = `${gridContainer.clientWidth/cellSize}px`;
+
+        //Add hover event to each cell
+        cell.addEventListener('mouseenter', function(){
+            cell.style.backgroundColor = 'red';
         })
+        gridContainer.appendChild(cell);
     }
 }
-createBoxes();
 
-//find box dimensions relative to the container
-const boxes = document.querySelectorAll(".box")
-
-function getBoxSize () {
-    boxes.forEach( (box) => {
-        const gridContainerArea = gridContainer.clientHeight * gridContainer.clientWidth;
-        console.log(gridContainerArea);
-        const boxWidthHeight = Math.sqrt(gridContainerArea/number);
-        console.log(boxWidthHeight);    box.style.width = `${boxWidthHeight}px`;
-
-        box.style.width = `${boxWidthHeight}px`;
-        box.style.height = `${boxWidthHeight}px`;    
-    })
-}
-
-getBoxSize();
-
-
-
-
-
-
-
-
-
+createCells();
