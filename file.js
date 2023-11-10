12ed0a544151d87b57974037151eecf03dc82b82
Baraka <barakaarunga@gmail.com>
2023-11-10 12:13:23 +0300

    Refactored code to remove repeatingly called functions. Fixed the button. It can now pass values to create grid function.

--- file.js
+++ file.js
@@ -1,29 +1,50 @@
 const gridContainer = document.querySelector('.grid-container');
+const main = document.querySelector('.main');
+const containerDimensions = gridContainer.clientHeight;
+console.log(`container height: ${containerDimensions}`)
 
-let cellSize = 32;
-let gridSize = cellSize*cellSize;
-console.log(gridSize);
+//create prompt button
+const promptBtn = document.createElement('button');
+promptBtn.className = 'prompt-btn';
+promptBtn.textContent = 'Create Grid';
+main.appendChild(promptBtn);
 
-//get cell size
-function getCellSize () {
-    prompt("Pick a grid size between 1 and 100")
+//clear grid to avoid stacking
+function clearGrid () {
+    gridContainer.innerHTML = '';
 }
 
+//create prompt
+function createGrid () {
+    clearGrid();
+    //get grid size
+    let gridNumber;
+    
+    do {
+        gridNumber = prompt("Pick a number between 10 and 100");
+        console.log(`gridNumber: ${gridNumber}`);
+    } while (isNaN(gridNumber) || gridNumber <  10 || gridNumber > 100);
 
-//create cells
-function createCells () {
-    for(let i = 0; i < gridSize; i++) {
-        const cell = document.createElement('div');
-        cell.className = "cell";
-        cell.style.width = `${gridContainer.clientWidth/cellSize}px`;
-        cell.style.height = `${gridContainer.clientWidth/cellSize}px`;
-
-        //Add hover event to each cell
-        cell.addEventListener('mouseenter', function(){
-            cell.style.backgroundColor = 'red';
-        })
-        gridContainer.appendChild(cell);
+    //add grid to DOM
+    const gridSize = gridNumber * gridNumber;
+    console.log(`gridsize: ${gridSize}`);
+    function generateGrid () {
+        for (let i = 0; i < gridSize; i++) {
+            const cell = document.createElement('div');
+            cell.className = 'cell';
+            cellWidthHeight = `${containerDimensions/gridNumber}`
+            console.log(cellWidthHeight)
+            cell.style.width = `${cellWidthHeight}px`;
+            cell.style.height = `${cellWidthHeight}px`;
+            cell.addEventListener('mouseenter', function () {
+                cell.style.backgroundColor = "red";
+            })
+            gridContainer.appendChild(cell);
+        }
     }
+
+    generateGrid();
+    
 }
 
-createCells();
+promptBtn.addEventListener('click', ()=> createGrid());
